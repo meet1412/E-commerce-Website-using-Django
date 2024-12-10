40da166238aea7a988563e5bc5bb153152761566
Meet Patel <patel.meet.5485@gmail.com>
2024-12-10 12:53:21 -0500

    basic structure of ecom website created

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ecom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'store/templates/store')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
