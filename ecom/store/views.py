8fae5960a385f810cdc26ff40fca3bccf05350d3
Meet Patel <patel.meet.5485@gmail.com>
2024-12-28 13:02:10 -0500

    guest checkout functionality added

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -28,12 +28,19 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+            print('CART:', cart)
+
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = order['get_cart_items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
     if request.user.is_authenticated:
