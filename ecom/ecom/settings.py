00dbe91a9f724393f3f4ad23d1dfb94b3d45a021
Meet Patel <patel.meet.5485@gmail.com>
2025-01-09 15:36:59 -0500

    Update settings.py

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -137,7 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
